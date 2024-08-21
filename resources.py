fd9ac3e00c2efe852384a76a3fcd242fbceb38d1
Peter Hudson <peter.t.hudson@outlook.com>
2024-08-21 20:40:51 +0100

    Added resource file

---
+++ resources.py
@@ -0,0 +1,8 @@
+#### COMMON VARIABLES ####
+
+# API Keys
+openWeatherAPIkey = "155b1cc2b1ccefcb5a370d6d01687f12"
+
+# API Endpoints
+openWeatherGeoCoding = 'http://api.openweathermap.org/geo/1.0/direct'
+openWeatherAggregate = 'https://api.openweathermap.org/data/3.0/onecall/day_summary'
